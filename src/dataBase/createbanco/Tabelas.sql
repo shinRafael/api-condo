db365e812a73361b5cf69fe368314aa341f6d269
shinRafael <ninjarafa55@gmail.com>
2025-05-22 21:25:50 -0300

    Correções no banco

--- src/dataBase/createbanco/Tabelas.sql
+++ src/dataBase/createbanco/Tabelas.sql
@@ -1,3 +1,4 @@
+-- 1. Condominio
 CREATE TABLE Condominio (
     cond_id INT AUTO_INCREMENT PRIMARY KEY,
     cond_nome VARCHAR(60) NOT NULL,
@@ -5,21 +6,7 @@ CREATE TABLE Condominio (
     cond_cidade VARCHAR(40) NOT NULL
 );
 
-CREATE TABLE Bloco (
-    bloc_id INT AUTO_INCREMENT PRIMARY KEY,
-    cond_id INT NOT NULL,
-    bloc_nome VARCHAR(60) NOT NULL,
-    FOREIGN KEY (cond_id) REFERENCES Condominio(cond_id)
-);
-
-CREATE TABLE Apartamentos (
-    ap_id INT AUTO_INCREMENT PRIMARY KEY,
-    bloco_id INT NOT NULL,
-    ap_numero VARCHAR(15) NOT NULL,
-    ap_andar INT NOT NULL,
-    FOREIGN KEY (bloco_id) REFERENCES Bloco(bloc_id)
-);
-
+-- 2. Usuarios
 CREATE TABLE Usuarios (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_nome VARCHAR(60) NOT NULL,
@@ -29,14 +16,7 @@ CREATE TABLE Usuarios (
     user_tipo VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Usuario_Apartamentos (
-    userap_id INT AUTO_INCREMENT PRIMARY KEY,
-    userid INT NOT NULL,
-    ap_id INT NOT NULL,
-    FOREIGN KEY (userid) REFERENCES Usuarios(user_id),
-    FOREIGN KEY (ap_id) REFERENCES Apartamentos(ap_id)
-);
-
+-- 3. Gerenciamento
 CREATE TABLE Gerenciamento (
     ger_id INT AUTO_INCREMENT PRIMARY KEY,
     cond_id INT NOT NULL,
@@ -46,6 +26,53 @@ CREATE TABLE Gerenciamento (
     FOREIGN KEY (cond_id) REFERENCES Condominio(cond_id)
 );
 
+-- 4. Bloco
+CREATE TABLE Bloco (
+    bloc_id INT AUTO_INCREMENT PRIMARY KEY,
+    cond_id INT NOT NULL,
+    bloc_nome VARCHAR(60) NOT NULL,
+    FOREIGN KEY (cond_id) REFERENCES Condominio(cond_id)
+);
+
+-- 5. Ambientes
+CREATE TABLE Ambientes (
+    amd_id INT AUTO_INCREMENT PRIMARY KEY,
+    cond_id INT NOT NULL,
+    amd_descricao VARCHAR(100) NOT NULL,
+    amd_nome VARCHAR(40) NOT NULL,
+    amd_capacidade INT NOT NULL,
+    FOREIGN KEY (cond_id) REFERENCES Condominio(cond_id)
+);
+
+-- 6. Apartamentos
+CREATE TABLE Apartamentos (
+    ap_id INT AUTO_INCREMENT PRIMARY KEY,
+    bloco_id INT NOT NULL,
+    ap_numero VARCHAR(15) NOT NULL,
+    ap_andar INT NOT NULL,
+    FOREIGN KEY (bloco_id) REFERENCES Bloco(bloc_id)
+);
+
+-- 7. Visitantes
+CREATE TABLE Visitantes (
+    vst_id INT AUTO_INCREMENT PRIMARY KEY,
+    vst_nome VARCHAR(60) NOT NULL,
+    vst_documento VARCHAR(60) NOT NULL,
+    ap_id INT NOT NULL,
+    vst_data_entrada DATETIME NOT NULL,  -- entrada na residência
+    vst_data_saida DATETIME NOT NULL,    -- saída da residência
+    FOREIGN KEY (ap_id) REFERENCES Apartamentos(ap_id)
+);
+-- 8. Usuario_Apartamentos
+CREATE TABLE Usuario_Apartamentos (
+    userap_id INT AUTO_INCREMENT PRIMARY KEY,
+    userid INT NOT NULL,
+    ap_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Usuarios(user_id),
+    FOREIGN KEY (ap_id) REFERENCES Apartamentos(ap_id)
+);
+
+-- 9. Mensagens
 CREATE TABLE Mensagens (
     msg_id INT AUTO_INCREMENT PRIMARY KEY,
     cond_id INT NOT NULL,
@@ -57,15 +84,17 @@ CREATE TABLE Mensagens (
     FOREIGN KEY (userap_id) REFERENCES Usuario_Apartamentos(userap_id)
 );
 
-CREATE TABLE Ambientes (
-    amd_id INT AUTO_INCREMENT PRIMARY KEY,
-    cond_id INT NOT NULL,
-    amd_descricao VARCHAR(100) NOT NULL,
-    amd_nome VARCHAR(40) NOT NULL,
-    amd_capacidade INT NOT NULL,
-    FOREIGN KEY (cond_id) REFERENCES Condominio(cond_id)
+-- 10. Notificacoes
+CREATE TABLE Notificacoes (
+    not_id INT AUTO_INCREMENT PRIMARY KEY,
+    userap_id INT NOT NULL,
+    not_mensagem TEXT NOT NULL,
+    not_data_envio DATETIME NOT NULL,
+    not_lida BOOLEAN NOT NULL,
+    FOREIGN KEY (userap_id) REFERENCES Usuario_Apartamentos(userap_id)
 );
 
+-- 11. Reservas_Ambientes
 CREATE TABLE Reservas_Ambientes (
     res_id INT AUTO_INCREMENT PRIMARY KEY,
     userap_id INT NOT NULL,
@@ -75,23 +104,5 @@ CREATE TABLE Reservas_Ambientes (
     res_status VARCHAR(25) NOT NULL,
     res_data_reserva DATE NOT NULL,
     FOREIGN KEY (userap_id) REFERENCES Usuario_Apartamentos(userap_id),
-    FOREIGN KEY (ambiid) REFERENCES Ambientes(amd_id)
-);
-
-CREATE TABLE Visitantes (
-    vst_id INT AUTO_INCREMENT PRIMARY KEY,
-    vst_nome VARCHAR(60) NOT NULL,
-    vst_documento VARCHAR(60) NOT NULL,
-    ap_id INT NOT NULL,
-    vst_data_visita DATETIME NOT NULL,
-    FOREIGN KEY (ap_id) REFERENCES Apartamentos(ap_id)
-);
-
-CREATE TABLE Notificacoes (
-    not_id INT AUTO_INCREMENT PRIMARY KEY,
-    userAP_id INT NOT NULL,
-    not_mensagem TEXT NOT NULL,
-    not_data_envio DATETIME NOT NULL,
-    not_lida BOOLEAN NOT NULL,
-    FOREIGN KEY (userAP_id) REFERENCES Usuario_Apartamentos(userap_id)
+    FOREIGN KEY (amb_id) REFERENCES Ambientes(amd_id)
 );

c76276c94e7e39022df57c84c9d41c6cdadb6cda
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-09-08 22:33:50 -0300

    add api ocorrencias

--- src/routes/routes-enc.js
+++ src/routes/routes-enc.js
@@ -5,22 +5,22 @@ const encomendasController = require('../controllers/encomendas')
 
 const ocorrenciasController = require('../controllers/ocorrencias')
 
-const documentosController = require('../controllers/')
+// const documentosController = require('../controllers/documentos')
 
 router.get('/encomendas', encomendasController.listarEncomendas);
 router.post('/encomendas', encomendasController.cadastrarEncomendas);
 router.patch('/encomendas/:id', encomendasController.editarEncomendas);
 router.delete('/encomendas/:id', encomendasController.apagarEncomendas);
 
-router.get('/ocorrencias', ocorrenciasController.listarOcorrencias);
-router.post('/ocorrencias', ocorrenciasController.cadastrarOcorrencias);
-router.patch('/ocorrencias/:id', ocorrenciasController.editarOcorrencias);
-router.delete('/ocorrencias/:id', ocorrenciasController.apagarOcorrencias);
+router.get('/ocorrencias', ocorrenciasController.listarocorrencias);
+router.post('/ocorrencias', ocorrenciasController.cadastrarocorrencias);
+router.patch('/ocorrencias/:id', ocorrenciasController.editarocorrencias);
+router.delete('/ocorrencias/:id', ocorrenciasController.apagarocorrencias);
 
-router.get('/documentos', documentosController.listarDocumentos);
-router.post('/documentos', documentosController.cadastrarDocumentos);
-router.patch('/documentos/:id', documentosController.editarDocumentos);
-router.delete('/documentos/:id', documentosController.apagarDocumentos);
+// router.get('/documentos', documentosController.listarDocumentos);
+// router.post('/documentos', documentosController.cadastrarDocumentos);
+// router.patch('/documentos/:id', documentosController.editarDocumentos);
+// router.delete('/documentos/:id', documentosController.apagarDocumentos);
 
 
 module.exports = router;

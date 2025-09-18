21631e0ba95dd23adde55242bba97042a943a075
shinRafael <ninjarafa55@gmail.com>
2025-09-18 20:10:56 -0300

    Agregando o inner join

--- src/controllers/gerenciamento.js
+++ src/controllers/gerenciamento.js
@@ -1,11 +1,18 @@
 const bd = require('../dataBase/connection');
 
 module.exports = {
-    async listargerenciamento (request, response){
+    async listargerenciamento(request, response) {
         try {
             const sql = `
-                SELECT ger_id, cond_id, ger_data, ger_descricao, ger_valor
-                FROM gerenciamento;
+                SELECT 
+                    ger.ger_id, 
+                    ger.cond_id, 
+                    cond.cond_nome, 
+                    DATE_FORMAT(ger.ger_data, '%d/%m/%Y') AS ger_data,
+                    ger.ger_descricao, 
+                    CONCAT('R$ ', FORMAT(ger.ger_valor, 2, 'pt_BR')) AS ger_valor
+                FROM gerenciamento ger 
+                INNER JOIN condominio cond ON ger.cond_id = cond.cond_id;
             `;
             const [rows] = await bd.query(sql);
 
@@ -15,7 +22,7 @@ module.exports = {
                 itens: rows.length,
                 dados: rows
             })
-        }catch (error) {
+        } catch (error) {
             return response.status(550).json({
                 sucesso: false,
                 mensagem: 'Erro na listagem de gerenciamento.',
@@ -23,13 +30,13 @@ module.exports = {
             });
         }
     },
-    async cadrastogerenciamento (request, response){
-        try{
-           const { cond_id, ger_data, ger_descricao, ger_valor } = request.body;
-           const ger_ativo = 1;
+    async cadrastogerenciamento(request, response) {
+        try {
+            const { cond_id, ger_data, ger_descricao, ger_valor } = request.body;
+            const ger_ativo = 1;
 
-           // instrução SQL
-           const sql = `
+            // instrução SQL
+            const sql = `
            INSERT INTO gerenciamento
                (cond_id, ger_data, ger_descricao, ger_valor)
            VALUES 
@@ -37,149 +44,149 @@ module.exports = {
        `;
 
 
-           // definição dos dados a serem inseridos em um array
-           const values = [cond_id, ger_data, ger_descricao, ger_valor];
+            // definição dos dados a serem inseridos em um array
+            const values = [cond_id, ger_data, ger_descricao, ger_valor];
 
-           //execução da instrução sql passando os parâmetros
-           const [result] = await bd.query(sql, values);
+            //execução da instrução sql passando os parâmetros
+            const [result] = await bd.query(sql, values);
 
-           //Identificação do ID do registro inserido 
-           const dados = {
-            id: result.insertId,
-            cond_id,
-            ger_data,
-            ger_descricao,
-            ger_valor
-        };
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: 'Cadastro de gerenciamento realizado.',
-            dados: dados
-        });
-        }catch (error){
+            //Identificação do ID do registro inserido 
+            const dados = {
+                id: result.insertId,
+                cond_id,
+                ger_data,
+                ger_descricao,
+                ger_valor
+            };
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Cadastro de gerenciamento realizado.',
+                dados: dados
+            });
+        } catch (error) {
             return response.status(550).json({
                 sucesso: false,
                 mensagem: 'Erro ao cadastrar de gerenciamento.',
                 dados: error.message
-             });
+            });
         }
     },
-    async editargerenciamento (request, response){
-        try{    //parâmetros recebidos pelo corpo da requisição 
-                const { cond_id, ger_data, ger_descricao, ger_valor } = request.body;
-                //parâmetro recebido pela URL via params ex: /usuario/1
-                const { id } = request.params;
-                // instrução SQL
-                const sql = `
+    async editargerenciamento(request, response) {
+        try {    //parâmetros recebidos pelo corpo da requisição 
+            const { cond_id, ger_data, ger_descricao, ger_valor } = request.body;
+            //parâmetro recebido pela URL via params ex: /usuario/1
+            const { id } = request.params;
+            // instrução SQL
+            const sql = `
                  UPDATE gerenciamento
                  SET cond_id = ?, ger_data = ?, ger_descricao = ?, ger_valor = ?
                  WHERE ger_id = ?
                  `;
-               //preparo do array com dados que serão atualizados 
-               const values = [cond_id, ger_data, ger_descricao, ger_valor, id];
-               //execução e obtenção de confirmação da atualização realizada
-               const [result] = await bd.query(sql, values);
+            //preparo do array com dados que serão atualizados 
+            const values = [cond_id, ger_data, ger_descricao, ger_valor, id];
+            //execução e obtenção de confirmação da atualização realizada
+            const [result] = await bd.query(sql, values);
 
-               // Verifica se alguma linha foi afetada
-        if (result.affectedRows === 0) {
-            return response.status(404).json({
+            // Verifica se alguma linha foi afetada
+            if (result.affectedRows === 0) {
+                return response.status(404).json({
+                    sucesso: false,
+                    mensagem: `Gerenciamento com ID ${id} não encontrado!`,
+                    dados: null
+                });
+            }
+
+            // Dados retornados para o frontend
+            const dados = {
+                id,
+                cond_id,
+                ger_data,
+                ger_descricao,
+                ger_valor
+            };
+
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Gerenciamento ${id} atualizado com sucesso!`,
+                dados
+            });
+
+        } catch (error) {
+            return response.status(500).json({
                 sucesso: false,
-                mensagem: `Gerenciamento com ID ${id} não encontrado!`,
-                dados: null
+                mensagem: 'Erro na requisição.',
+                dados: error.message
             });
         }
+    },
+    async apagargerenciamento(request, response) {
+        try {
+            // parâmetro passado via URL na chamada da API pelo front-end
+            const { id } = request.params;
 
-        // Dados retornados para o frontend
-        const dados = {
-            id,
-            cond_id,
-            ger_data,
-            ger_descricao,
-            ger_valor
-        };
+            // comando de exclusão
+            const sql = 'DELETE FROM gerenciamento WHERE ger_id = ?';
 
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: `Gerenciamento ${id} atualizado com sucesso!`,
-            dados
-        });
+            // array com parâmetros da exclusão
+            const values = [id];
 
-    } catch (error) {
-        return response.status(500).json({
-            sucesso: false,
-            mensagem: 'Erro na requisição.',
-            dados: error.message
-        });
+            // executa instrução no banco de dados
+            const [result] = await bd.query(sql, values);
+
+            // se nenhum registro foi afetado, o item não foi encontrado
+            if (result.affectedRows === 0) {
+                return response.status(404).json({
+                    sucesso: false,
+                    mensagem: `Serviço ${id} não encontrado!`,
+                    dados: null
+                });
+            }
+
+            // retorno de sucesso
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Serviço ${id} excluído com sucesso`,
+                dados: null
+            });
+
+        } catch (error) {
+            // erro na requisição
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async filtrarGerenciamento(request, response) {
+        try {
+            const { tipo, condominio } = request.query;
+
+            let sql = 'SELECT * FROM gerenciamento WHERE 1=1';
+            const params = [];
+
+            if (tipo) {
+                sql += 'AND tipo_usuario = ?';
+                params.push(tipo);
+            }
+
+            if (condominio) {
+                sql += 'AND cond_id = ?';
+                params.push(condominio);
+            }
+            const [rows] = await bd.query(sql, params);
+
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Gerenciamentos filtrados com sucessos!',
+                dados: rows
+            });
+        } catch (error) {
+            response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro ao filtrar gerenciamento.',
+                dados: error.message
+            });
+        }
     }
-},
-async apagargerenciamento(request, response) {
-    try {
-        // parâmetro passado via URL na chamada da API pelo front-end
-        const { id } = request.params;
-
-        // comando de exclusão
-        const sql = 'DELETE FROM gerenciamento WHERE ger_id = ?';
-
-        // array com parâmetros da exclusão
-        const values = [id];
-
-        // executa instrução no banco de dados
-        const [result] = await bd.query(sql, values);
-
-        // se nenhum registro foi afetado, o item não foi encontrado
-        if (result.affectedRows === 0) {
-            return response.status(404).json({
-                sucesso: false,
-                mensagem: `Serviço ${id} não encontrado!`,
-                dados: null
-            });
-        }
-
-        // retorno de sucesso
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: `Serviço ${id} excluído com sucesso`,
-            dados: null
-        });
-
-     } catch (error) {
-        // erro na requisição
-        return response.status(500).json({
-            sucesso: false,
-            mensagem: 'Erro na requisição.',
-            dados: error.message
-        });
-      }
-   },
-   async filtrarGerenciamento(request, response){
-    try {
-        const { tipo, condominio } = request.query;
-
-        let sql = 'SELECT * FROM gerenciamento WHERE 1=1';
-        const params = [];
-
-        if (tipo) {
-            sql += 'AND tipo_usuario = ?';
-            params.push(tipo);
-        }
-
-        if (condominio) { 
-            sql += 'AND cond_id = ?';
-            params.push(condominio);
-        }
-        const [rows] = await bd.query(sql, params);
-
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: 'Gerenciamentos filtrados com sucessos!',
-            dados: rows 
-        });
-    } catch (error) {
-        response.status(500).json({
-        sucesso: false,
-        mensagem: 'Erro ao filtrar gerenciamento.',
-        dados: error.message 
-        });
-      }   
-   }
 };       

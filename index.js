2911174e3b1f89a92b894de1df5b9d10cf6c0e0c
Matheus <90573780+MatheusVRibeiro@users.noreply.github.com>
2025-10-01 19:54:52 -0300

    att index.

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-require('dotenv').config();
-const express = require('express'); 
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const router = require('./src/routes/routes');
+const router = require("./src/routes/routes");
 
 const app = express();
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
-
-// importa as rotas
-const routes = require('./src/routes/routes-rafael');
-app.use('/', routes); // todas as rotas já estão com prefixo (/condominio, /gerenciamento, etc.)
+app.use(router);
 
 // inicia servidor
 const porta = process.env.PORT || 3333;
 app.listen(porta, () => {
-    console.log(`Servidor iniciado em http://localhost:${porta}`);
+  console.log(`Servidor iniciado em http://localhost:${porta}`);
 });
 
-// rota de teste
-app.get('/', (request, response) => {
-    response.send('Hello World');
-
+app.get("/", (request, response) => {
+  response.send("Hello World");
 });
